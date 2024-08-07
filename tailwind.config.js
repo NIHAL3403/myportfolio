f78f48c32fcace7d785d4e7ac8a9af87cef19529
NIHAL3403 <106047554+NIHAL3403@users.noreply.github.com>
2024-08-07 18:13:15 +0530

    code running successfully

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html"
+  ],
+  darkMode: 'class', // Enable dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
